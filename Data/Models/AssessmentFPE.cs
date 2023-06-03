19ea9c2ae7b0eafc1a4b7e78a0770e7a5eff9a1c
Navid Manoochehri <nmanoochehri@gmail.com>
2023-06-03 16:20:19 +0330

    Edit

---
+++ Data/Models/AssessmentFPE.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Drawing;
+
+
+namespace Tamin.Data.Models
+{
+    public class AssessmentFPE
+    {
+        public int Id { get; set; }
+        public int Resume { get; set; }
+        public int Price { get; set; }
+        public int Customer { get; set; }
+        public int Financial { get; set; }
+        public int Technical { get; set; }
+        public int Professionalism { get; set; }
+        public string Project { get; set; }
+        public int Sum { get; set; }
+        public string Grade { get; set; }
+        public string Date { get; set; }
+        public string MDate { get; set; }
+        public string Comment { get; set; }
+        public byte[] Image { get; set; }
+             
+    }
+}
---
+++ Data/Models/AssessmentSPE.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Drawing;
+
+namespace Tamin.Data.Models
+{
+    public class AssessmentSPE
+    {
+        public int Id { get; set; }
+        public int Timing { get; set; }
+        public int Quality { get; set; }
+        public int Standard { get; set; }
+        public int Equipment_protection { get; set; }
+        public int Material_protection { get; set; }
+        public int Protection_consumables { get; set; }
+        public int Resource_management { get; set; }
+        public int Technical_ability { get; set; }
+        public int Professionalism { get; set; }
+        public int Status_face { get; set; }
+        public int Correct_use { get; set; }
+        public int Material_balance { get; set; }
+        public string Project { get; set; }
+        public int Sum { get; set; }
+        public string Grade { get; set; }
+        public string Date { get; set; }
+        public string MDate { get; set; }
+        public string Comment { get; set; }
+        public byte[] Image { get; set; }
+
+
+    }
+}
